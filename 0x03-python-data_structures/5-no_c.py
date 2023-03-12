639bd220feaf7166e977126aafc1b7d6a5c24d66
DrRay22 <scengltd@gmail.com>
2023-03-12 08:28:45 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return
-"""
-
 def no_c(my_string):
     new_string = ""
     for char in my_string:
